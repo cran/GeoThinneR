8625675367c4764091e59c2a4d2d1b0e2573e311
Jorge Mestre-Tomás <jorge.mestre.tomas@csic.es>
2025-04-24 06:10:02 +0000

    version 2.0.0

--- inst/doc/GeoThinneR.R
+++ inst/doc/GeoThinneR.R
@@ -18,32 +18,32 @@ library(ggplot2)
 # Set seed for reproducibility
 set.seed(123)
 
-# Simulate the dataset
-n <- 2000  # Number of points
+# Simulated dataset
+n <- 2000
 sim_data <- data.frame(
-  long = runif(n, min = -20, max = 20),
-  lat = runif(n, min = -10, max = 10),
+  lon = runif(n, min = -10, max = 10),
+  lat = runif(n, min = -5, max = 5),
   sp = sample(c("sp1", "sp2"), n, replace = TRUE)
 )
 
-# Load the Caretta caretta occurrences
+# Load real-world occurrence dataset
 data("caretta")
 
-# Load mediterranean sea polygon
+# Load Mediterranean Sea polygon
 medit <- system.file("extdata", "mediterranean_sea.gpkg", package = "GeoThinneR")
 medit <- sf::st_read(medit)
 
-## ----plot loaded data, , fig.show="hold",  out.width="50%"--------------------
+## ----plot loaded data, fig.show="hold", out.width="50%", echo = FALSE---------
 ggplot() +
-  geom_point(data = sim_data, aes(x = long, y = lat, color = sp)) +
-  scale_color_manual(values = c(sp1 = "#5183B3", sp2 = "#EB714B")) +
+  geom_point(data = sim_data, aes(x = lon, y = lat, shape = sp), color = "#5183B3") +
+  scale_shape_manual(values = c(sp1 = 16, sp2 = 17)) +
   xlab("Longitude") + ylab("Latitude") +
   ggtitle("Simulated Species Occurrences") +
   theme_minimal()
 
 ggplot() +
   geom_sf(data = medit, color = "#353839", fill = "antiquewhite", alpha = 0.7)  +
-  geom_point(data = caretta, aes(x = decimalLongitude, y = decimalLatitude),color = "#EB714B", alpha = 1) +
+  geom_point(data = caretta, aes(x = decimalLongitude, y = decimalLatitude), color = "#5183B3", alpha = 1) +
   xlab("Longitude") + ylab("Latitude") +
   ggtitle("C. caretta Mediterranean Sea Occurrences") +
   theme(
@@ -55,40 +55,64 @@ ggplot() +
 
 ## -----------------------------------------------------------------------------
 # Apply spatial thinning to the simulated data
-thin_sim_data <- thin_points(
-  data = sim_data, # Dataframe with coordinates
-  long_col = "long", # Longitude column name
-  lat_col = "lat", # Latitude column name
-  method = "brute_force",  # Method for thinning
-  thin_dist = 20,  # Thinning distance in km,
-  trials = 5, # Number of reps
-  all_trials = TRUE, # Return all trials
-  seed = 123 # Seed for reproducibility
+quick_thin <- thin_points(
+  data = sim_data,     # Dataframe with coordinates
+  lon_col = "lon",     # Longitude column name
+  lat_col = "lat",     # Latitude column name
+  method = "distance", # Method for thinning
+  thin_dist = 20,      # Thinning distance in km,
+  trials = 5,          # Number of trials
+  all_trials = TRUE,   # Return all trials
+  seed = 123           # Seed for reproducibility
 )
 
+quick_thin
+
 ## -----------------------------------------------------------------------------
-# Number of keeped points in each trial
-sapply(thin_sim_data, nrow)
+summary(quick_thin)
+
+## -----------------------------------------------------------------------------
+plot(quick_thin)
+
+## -----------------------------------------------------------------------------
+# Number of kept points in each trial
+sapply(quick_thin$retained, sum)
+
+## -----------------------------------------------------------------------------
+head(largest(quick_thin))
+
+## -----------------------------------------------------------------------------
+head(get_trial(quick_thin, trial = 2)) 
+
+## -----------------------------------------------------------------------------
+trial <- 2
+head(quick_thin$original_data[!quick_thin$retained[[trial]], ])
+
+## -----------------------------------------------------------------------------
+sf_thinned <- as_sf(quick_thin)
+class(sf_thinned)
 
 ## -----------------------------------------------------------------------------
 # Apply spatial thinning to the real data
-thin_real_data <- thin_points(
-  data = caretta, # We will not specify long_col, lat_col as they are in position 1 and 2
-  method = "kd_tree",
-  thin_dist = 30,  # Thinning distance in km,
+caretta_thin <- thin_points(
+  data = caretta, # We will not specify lon_col, lat_col as they are in position 1 and 2
+  lat_col = "decimalLatitude",
+  lon_col = "decimalLongitude",
+  method = "distance",
+  thin_dist = 30, # Thinning distance in km,
   trials = 5,
   all_trials = FALSE,
   seed = 123
 )
 
-# Thinned dataframe stored in the first element of the output list
-dim(thin_real_data[[1]])
+# Thinned dataframe stored in the first element of the retained list
+identical(largest(caretta_thin), get_trial(caretta_thin, 1))
 
 ## ----echo = FALSE-------------------------------------------------------------
 ggplot() +
   geom_sf(data = medit, color = "#353839", fill = "antiquewhite", alpha = 0.7)  +
   geom_point(data = caretta, aes(x = decimalLongitude, y = decimalLatitude),color = "#EB714B", alpha = 0.5) +
-  geom_point(data = thin_real_data[[1]], aes(x = long, y = lat),color = "#5183B3", alpha = 1) +
+  geom_point(data = largest(caretta_thin), aes(x = decimalLongitude, y = decimalLatitude),color = "#5183B3", alpha = 1) +
   xlab("Longitude") + ylab("Latitude") +
   ggtitle("C. caretta Mediterranean Sea Occurrences") +
   theme(
@@ -99,78 +123,102 @@ ggplot() +
     )
 
 ## -----------------------------------------------------------------------------
-system.time(
-thin_sim_data <- thin_points(
+dist_thin <- thin_points(sim_data, method = "distance", thin_dist = 25, trials = 3)
+plot(dist_thin)
+
+## -----------------------------------------------------------------------------
+brute_thin <- thin_points(
   data = sim_data,
-  method = "brute_force", 
+  method = "distance",
+  search_type = "brute",
   thin_dist = 20,
-  trials = 50,
+  trials = 10,
   all_trials = FALSE,
   seed = 123
-))
-nrow(thin_sim_data[[1]])
+)
+nrow(largest(brute_thin))
+
+## -----------------------------------------------------------------------------
+kd_tree_thin <- thin_points(
+  data = sim_data,
+  method = "distance",
+  search_type = "kd_tree",
+  thin_dist = 20,
+  trials = 10,
+  all_trials = FALSE,
+  seed = 123
+)
+nrow(largest(kd_tree_thin))
+
+## -----------------------------------------------------------------------------
+local_kd_tree_thin <- thin_points(
+  data = sim_data,
+  method = "distance",
+  search_type = "local_kd_tree",
+  thin_dist = 20,
+  trials = 10,
+  all_trials = FALSE,
+  seed = 123
+)
+nrow(largest(local_kd_tree_thin))
+
+## -----------------------------------------------------------------------------
+k_estimation_thin <- thin_points(
+  data = sim_data,
+  method = "distance",
+  search_type = "k_estimation",
+  thin_dist = 20,
+  trials = 10,
+  all_trials = FALSE,
+  seed = 123
+)
+nrow(largest(k_estimation_thin))
 
 ## -----------------------------------------------------------------------------
 system.time(
-thin_sim_data <- thin_points(
+grid_thin <- thin_points(
   data = sim_data,
-  method = "kd_tree", 
-  thin_dist = 20,
-  trials = 50,
-  all_trials = FALSE,
-  seed = 123
-))
-nrow(thin_sim_data[[1]])
-
-## -----------------------------------------------------------------------------
-system.time(
-thin_sim_data <- thin_points(
-  data = sim_data,
-  method = "round_hash", 
-  thin_dist = 20,
-  trials = 50,
-  all_trials = FALSE,
-  seed = 123
-))
-nrow(thin_sim_data[[1]])
-
-## -----------------------------------------------------------------------------
-system.time(
-thin_sim_data <- thin_points(
-  data = sim_data,
-  method = "grid", 
+  method = "grid",
   resolution = 1,
   origin = c(0, 0),
   crs = "epsg:4326",
-  trials = 50,
+  n = 1, # one point per grid cell
+  trials = 10,
   all_trials = FALSE,
   seed = 123
 ))
+nrow(largest(grid_thin))
 
 ## ----message=FALSE, warning=FALSE---------------------------------------------
-rast_obj <- terra::rast(xmin = -20, xmax = 20, ymin = -10, ymax = 10, res = 1)
+rast_obj <- terra::rast(xmin = -10, xmax = 10, ymin = -5, ymax = 5, res = 1)
 system.time(
-thin_sim_data <- thin_points(
+grid_raster_thin <- thin_points(
   data = sim_data,
   method = "grid", 
   raster_obj = rast_obj,
-  trials = 50,
+  trials = 10,
+  n = 1,
   all_trials = FALSE,
   seed = 123
 ))
+nrow(largest(grid_raster_thin))
 
 ## ----echo = FALSE-------------------------------------------------------------
 rast_obj <- terra::as.polygons(rast_obj)
 rast_obj <- sf::st_as_sf(rast_obj)
 ggplot() +
   geom_sf(data = rast_obj, color = "#353839") +
-  geom_point(data = sim_data, aes(x = long, y = lat),color = "#EB714B", alpha = 0.5) +
-  geom_point(data = thin_sim_data[[1]], aes(x = long, y = lat),color = "#5183B3", alpha = 1) +
+  geom_point(data = sim_data, aes(x = lon, y = lat),color = "#EB714B", alpha = 0.5) +
+  geom_point(data = largest(grid_raster_thin), aes(x = lon, y = lat),color = "#5183B3", alpha = 1) +
+  labs(title = "Grid-based thinning results",
+       x = "Longitude",
+       y = "Latitude",
+       caption = "Blue points are kept, red points are deleted") +
   theme_minimal()
 
 ## -----------------------------------------------------------------------------
 system.time(
-thin_sim_data <- thin_points(
+precision_thin <- thin_points(
   data = sim_data,
   method = "precision", 
   precision = 0,
@@ -178,59 +226,71 @@ thin_sim_data <- thin_points(
   all_trials = FALSE,
   seed = 123
 ))
-nrow(thin_sim_data[[1]])
+nrow(largest(precision_thin))
+
+## ----echo = FALSE-------------------------------------------------------------
+ggplot() +
+  geom_point(data = sim_data, aes(x = lon, y = lat),  color = "#EB714B", size = 3, alpha = 0.5) +
+  geom_point(data = largest(precision_thin), aes(x = lon, y = lat), color = "#5183B3", size = 3) +
+  labs(title = "Precision-based thinning results",
+       x = "Longitude",
+       y = "Latitude",
+       caption = "Blue points are kept, red points are deleted") +
+  theme_minimal()
 
 ## -----------------------------------------------------------------------------
-thin_sim_data <- thin_points(
+all_thin <- thin_points(
   data = sim_data,
-  thin_dist = 20,
+  thin_dist = 100,
   seed = 123
 )
-thin_sim_data_group <- thin_points(
+grouped_thin <- thin_points(
   data = sim_data,
   group_col = "sp",
-  thin_dist = 20,
+  thin_dist = 100,
   seed = 123
 )
 
-nrow(thin_sim_data[[1]])
-nrow(thin_sim_data_group[[1]])
+nrow(largest(all_thin))
+nrow(largest(grouped_thin))
 
 ## ----echo =FALSE, fig.show="hold",  out.width="50%"---------------------------
-removed <- sim_data[-as.numeric(rownames(thin_sim_data[[1]])), ]
-removed_group <- sim_data[-as.numeric(rownames(thin_sim_data_group[[1]])), ]
+removed <- sim_data[!all_thin$retained[[1]], ]
+removed_group <- sim_data[!grouped_thin$retained[[1]], ]
 
 ggplot() +
-  geom_point(data = sim_data, aes(x = long, y = lat, color = sp), alpha = 0.2) +
-  geom_point(data = removed, aes(x = long, y = lat, color = sp), alpha = 1) +
-  scale_color_manual(values = c(sp1 = "#5183B3", sp2 = "#EB714B")) +
-  ggtitle("Removed points without grouping") +
+  geom_point(data = removed, aes(x = lon, y = lat, shape = sp), color = "#EB714B",  alpha = 0.2) +
+  geom_point(data = largest(all_thin), aes(x = lon, y = lat, shape = sp), color = "#5183B3", alpha = 1) +
+  scale_shape_manual(values = c(sp1 = 15, sp2 = 17)) +
+  ggtitle("Thinning without grouping") +
   theme_minimal()
 
 ggplot() +
-  geom_point(data = sim_data, aes(x = long, y = lat, color = sp), alpha = 0.2) +
-  geom_point(data = removed_group, aes(x = long, y = lat, color = sp), alpha = 1) +
-  scale_color_manual(values = c(sp1 = "#5183B3", sp2 = "#EB714B")) +
-  ggtitle("Removed points for each independent species") +
+  geom_point(data = removed_group, aes(x = lon, y = lat, shape = sp), color = "#EB714B",  alpha = 0.2) +
+  geom_point(data = largest(grouped_thin), aes(x = lon, y = lat, shape = sp), color = "#5183B3", alpha = 1) +
+  scale_shape_manual(values = c(sp1 = 15, sp2 = 17)) +
+  ggtitle("Thinning by group") +
   theme_minimal()
 
 ## -----------------------------------------------------------------------------
-thin_real_data <- thin_points(
-  data = caretta, 
+targeted_thin <- thin_points(
+  data = caretta,
+  lon_col = "decimalLongitude",
+  lat_col = "decimalLatitude",
   target_points = 150,
   thin_dist = 30,
   all_trials = FALSE,
   seed = 123,
   verbose = TRUE
 )
-nrow(thin_real_data[[1]])
+nrow(largest(targeted_thin))
 
-## -----------------------------------------------------------------------------
+## ----echo = FALSE-------------------------------------------------------------
 ggplot() +
   geom_sf(data = medit, color = "#353839", fill = "antiquewhite", alpha = 0.7)  +
-  geom_point(data = thin_real_data[[1]], aes(x = long, y = lat),color = "#5183B3", alpha = 1) +
+  geom_point(data = largest(targeted_thin), aes(x = decimalLongitude, y = decimalLatitude),color = "#5183B3", alpha = 1) +
   xlab("Longitude") + ylab("Latitude") +
-  ggtitle("C. caretta Mediterranean Sea Occurrences") +
+  ggtitle("Exact number of occurrences") +
   theme(
       panel.grid.major = element_line(color = gray(.5),linetype = "dashed",linewidth = 0.5),
       panel.background = element_rect(fill = "white"),
@@ -239,25 +299,29 @@ ggplot() +
     )
 
 ## -----------------------------------------------------------------------------
-thin_real_data <- thin_points(
+grid_thin <- thin_points(
   data = caretta,
-  method = "precision",
-  precision = 0,
+  lon_col = "decimalLongitude",
+  lat_col = "decimalLatitude",
+  method = "grid",
+  resolution = 2,
   seed = 123
 )
 
 # Substracting the maximum - the highest uncertainty becomes the lowest priority and vice versa.
-priority <- max(caretta$coordinateUncertaintyInMeters) - caretta$coordinateUncertaintyInMeters
-thin_real_data_uncert <- thin_points(
+priority <- max(caretta$coordinateUncertaintyInMeters, na.rm = TRUE) - caretta$coordinateUncertaintyInMeters
+priority_thin <- thin_points(
   data = caretta,
-  method = "precision",
-  precision = 0,
+  lon_col = "decimalLongitude",
+  lat_col = "decimalLatitude",
+  method = "grid",
+  resolution = 2,
   priority = priority,
   seed = 123
 )
 
-mean(thin_real_data[[1]]$coordinateUncertaintyInMeters)
-mean(thin_real_data_uncert[[1]]$coordinateUncertaintyInMeters)
+mean(largest(grid_thin)$coordinateUncertaintyInMeters, na.rm = TRUE)
+mean(largest(priority_thin )$coordinateUncertaintyInMeters, na.rm = TRUE)
 
 ## -----------------------------------------------------------------------------
 sessionInfo()
